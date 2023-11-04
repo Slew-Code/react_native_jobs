7d9afdee609ab98887cb9cefc305ddf90cd69c1b
Samuel Lewin <65896274+Slew-Code@users.noreply.github.com>
2023-11-04 12:40:21 +0000

    Add drawer navigation panel

--- app/index.js
+++ app/index.js
@@ -4,72 +4,82 @@ import { Stack, useRouter } from "expo-router";
 
 import { COLORS, icons, images, SIZES } from "../constants";
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome,} from "../components";
-//import NewCard from "./NewCard";
-import PopularJobCard from "../components/common/cards/popular/PopularJobCard";
 
-const Home = () => {
-        const router = useRouter();
-        const [searchTerm, setSearchTerm] = useState("");
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { NavigationContainer } from '@react-navigation/native';
 
-        const handleHeaderLeftClick = () => {
-            // Use router.push to navigate to the NewCard screen when headerRight is clicked
-            //router.push("PopularJobCard"); // Replace "NewCard" with the actual name you assigned to your card screen
-            console.log("Left");
-        };        
+function Home({ navigation }) {
+    const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState("");  
 
-        const handleHeaderRightClick = () => {
-            // Use router.push to navigate to the NewCard screen when headerRight is clicked
-            //router.push("PopularJobCard"); // Replace "NewCard" with the actual name you assigned to your card screen
-            console.log("Right");
-        };
+    const handleHeaderLeftClick = () => {
+        // Use router.push to navigate to the NewCard screen when headerRight is clicked
+        //router.push("PopularJobCard"); // Replace "NewCard" with the actual name you assigned to your card screen
+        console.log("Left");
+        navigation.openDrawer();
+    };       
 
-        return (
-            // shows content safely without overlap 
-            <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-                <Stack.Screen
-                    options={{
-                        headerStyle: { backgroundColor: COLORS.lightWhite },
-                        headerShadowVisible: false,
-                        headerLeft: () => (
-                            <ScreenHeaderBtn 
-                            iconUrl={icons.menu} dimension="60%" 
-                            handlePress={handleHeaderLeftClick}
-                            />
-                        ),
-                        headerRight: () => (
-                            <ScreenHeaderBtn
-                                iconUrl={images.profile} dimension="100%"
-                                handlePress={handleHeaderRightClick} // Call the function when headerRight is clicked
-                            />
-                        ),
-                        headerTitle: "",
-                    }}
-                />
-
-                
-                <ScrollView showsVerticalScrollIndicator={false}>
-                    <View
-                        style={{
-                            flex: 1,
-                            padding: SIZES.medium,
-                        }}
-                    >
-                        <Welcome
-                            searchTerm={searchTerm}
-                            setSearchTerm={setSearchTerm}
-                            handleClick={() => {
-                                if (searchTerm) {
-                                    router.push(`/search/${searchTerm}`)
-                                }
-                            }}
-                        />
-
-                        <Popularjobs />
-                        <Nearbyjobs />
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        );
+    const handleHeaderRightClick = () => {
+        // Use router.push to navigate to the NewCard screen when headerRight is clicked
+        //router.push("PopularJobCard"); // Replace "NewCard" with the actual name you assigned to your card screen
+        console.log("Right");
+        
     };
 
-export default Home;
+    return (
+        // shows content safely without overlap 
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn 
+                        iconUrl={icons.menu} dimension="60%" 
+                        handlePress={handleHeaderLeftClick}
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={images.profile} dimension="100%"
+                            handlePress={handleHeaderRightClick} // Call the function when headerRight is clicked
+                        />
+                    ),
+                    headerTitle: "",
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                    }}
+                >
+                    <Welcome
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        handleClick={() => {
+                            if (searchTerm) {
+                                router.push(`/search/${searchTerm}`)
+                            }
+                        }}
+                    />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+    return (       
+        <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home} />    
+        </Drawer.Navigator>
+    );
+}
