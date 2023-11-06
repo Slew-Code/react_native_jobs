c469539e68fe9fc914975b5c1aaf18f99d210559
Samuel Lewin <65896274+Slew-Code@users.noreply.github.com>
2023-11-06 20:39:57 +0000

    Remove mmkv storage code cause i cant get it to work :(

--- app/index.js
+++ app/index.js
@@ -77,6 +77,7 @@ function Home({ navigation }) {
 };
 
 function NotificationsScreen({ navigation }) {
+      
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Button onPress={() => navigation.goBack()} title="Go back home" />
--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -3,12 +3,7 @@ import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
 import styles from "./footer.style";
 import { icons } from "../../../constants";
 
-//import { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
-
-//const MMKV = new MMKVLoader().initialize();
-
 const Footer = ({ url }) => {
-  //const [user, setUser] = useMMKVStorage("user", MMKV, "robert");
   
   return (
     <View style={styles.container}>
@@ -18,7 +13,6 @@ const Footer = ({ url }) => {
           resizeMode='contain'
           style={styles.likeBtnImage}
           // implement a simple storage for react native to store liked jobs 
-          //onPress={() => { setUser("andrew"); }}
         />
       </TouchableOpacity>
 
